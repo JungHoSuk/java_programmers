e884f343b47b54428eff60c6db22fc2e5266fe6b
정호석 <wjdghtjr5345@gmail.com>
2023-02-25 19:07:25 +0900

    양꼬치

---
+++ level0/src/level0/LambSkewers.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class LambSkewers {
+
+	public static void main(String[] args) {
+		int n = 10;
+		int k = 3;
+		int answer = 0;
+		
+		answer =  n*12000 + k*2000 - (n/10)*2000;
+		System.out.println(answer);
+	}
+
+}
