319ba8b8dcfdbf7f26b33dc2e409a25bf0cabebd
정호석 <wjdghtjr5345@gmail.com>
2023-02-26 17:43:32 +0900

    순서쌍의 개수

---
+++ level0/src/level0/NumberOfOrderedPairs.java
@@ -0,0 +1,19 @@
+package level0;
+
+public class NumberOfOrderedPairs {
+
+	public static void main(String[] args) {
+		int n = 1201;
+		int answer = 0;
+		
+		for(int i=1;i<=n;i++) {
+			if(n%i==0){
+				answer++;
+			}
+		}
+		System.out.println(answer);
+		answer*= 2;
+		System.out.println(answer);
+	}
+
+}
