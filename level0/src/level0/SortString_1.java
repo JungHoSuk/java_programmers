a616754c0bc8b8ff58eedfa104a67ee0a5e88032
정호석 <wjdghtjr5345@gmail.com>
2023-03-20 20:17:54 +0900

    문자열 정렬하기(1)

---
+++ level0/src/level0/SortString_1.java
@@ -0,0 +1,34 @@
+package level0;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class SortString_1 {
+
+	public static void main(String[] args) {
+		String my_string = "adfadf1253";
+		int[] answer;
+		int cnt = 0;
+		String test = "";
+		ArrayList<Integer> arrAnswer = new ArrayList<Integer>();
+		for(int i=0;i<my_string.length();i++) {
+			if( my_string.charAt(i) > 47 && my_string.charAt(i) < 58) {
+				cnt++;
+				System.out.println(my_string.charAt(i));
+				arrAnswer.add(Integer.valueOf(my_string.charAt(i))-48);
+			}
+		}
+		System.out.println(arrAnswer.toString());
+		
+		answer = new int[cnt];
+		arrAnswer.sort(null);
+		System.out.println(arrAnswer.toString());
+		for(int i=0;i<arrAnswer.size();i++) {
+			answer[i] = arrAnswer.get(i);
+		}
+		System.out.println(Arrays.toString(answer));
+	}
+
+}
