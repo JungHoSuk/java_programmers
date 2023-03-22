df4cf664fb2fafb48227553c750ed512f6693cd8
정호석 <wjdghtjr5345@gmail.com>
2023-03-22 22:56:11 +0900

    종이 자르기

---
+++ level0/src/level0/PaperCut.java
@@ -0,0 +1,13 @@
+package level0;
+
+public class PaperCut {
+
+	public static void main(String[] args) {
+		int M = 5;
+		int N = 2;
+		int answer = 0;
+		if(M*N != 1) { answer = M*N-1;}
+		
+	}
+
+}
