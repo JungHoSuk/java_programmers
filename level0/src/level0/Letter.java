e05f524a01e333e77e068a685b54d7109351de14
정호석 <wjdghtjr5345@gmail.com>
2023-03-24 16:16:01 +0900

    편지

---
+++ level0/src/level0/Letter.java
@@ -0,0 +1,13 @@
+package level0;
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String message = "hello";
+		int answer = 0;
+		
+		answer = message.length()*2;
+		System.out.println(answer);
+	}
+
+}
