f536e9ed98ea70b1528dc7362af1c61c99e9d407
정호석 <wjdghtjr5345@gmail.com>
2023-03-24 15:32:32 +0900

    약수 구하기

---
+++ level0/src/level0/GetDivisor.java
@@ -0,0 +1,22 @@
+package level0;
+
+import java.util.Arrays;
+
+public class GetDivisor {
+
+	public static void main(String[] args) {
+		int n= 20;
+		String strAnswer = "";
+		int[] answer = null;
+		for(int i=1;i<=n;i++) {
+			if(n%i==0) {strAnswer+= (char)i;}
+		}
+//		System.out.println(strAnswer);
+		answer = new int[strAnswer.length()];
+		for(int i=0;i<strAnswer.length();i++) {
+			answer[i] = strAnswer.charAt(i);
+	
+		}
+		System.out.println(Arrays.toString(answer));
+	}
+}
