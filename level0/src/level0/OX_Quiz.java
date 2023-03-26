fbd6fd316b94d035687852ccf5b4051cc558bb45
정호석 <wjdghtjr5345@gmail.com>
2023-03-26 15:40:15 +0900

    OX퀴즈

--- level0/src/level0/OX_Quiz.java
+++ level0/src/level0/OX_Quiz.java
@@ -6,13 +6,20 @@ public class OX_Quiz {
 
 	public static void main(String[] args) {
 
-		String[] answer = {"3 - 4 = -3","5 + 6 = 11"};
-		String[] answer2 = null;
-		String x,y,z;
-		for(int i=0;i<answer.length;i++) {
-			answer2 = answer[i].split(" ");
-			System.out.println(Arrays.toString(answer2));
+		String[] quiz = {"3 - 4 = -3","5 + 6 = 11"};
+		String[] splitQuiz = null;
+		String[] answer = new String[quiz.length];
+		for(int i=0;i<quiz.length;i++) {
+			splitQuiz = quiz[i].split(" ");
+			System.out.println(Arrays.toString(splitQuiz));
+			if(splitQuiz[1].equals("+")) {
+				answer[i] = Integer.valueOf(splitQuiz[0]) + Integer.valueOf(splitQuiz[2]) == Integer.valueOf(splitQuiz[4])?"O":"X";
+			}else {
+				answer[i] = Integer.valueOf(splitQuiz[0]) - Integer.valueOf(splitQuiz[2]) == Integer.valueOf(splitQuiz[4])?"O":"X";
+			}
+			
 		}
+		System.out.println(Arrays.toString(answer));
 	}
 
 }
